ae9177be720fe08d8636d3ff0387d3a3aed4a2df
dvdt5 <dagtekindavut07@gmail.com>
2025-09-09 16:06:49 +0300

    added views and controller for product

--- InventoryManagementSystem/Controllers/ProductController.cs
+++ InventoryManagementSystem/Controllers/ProductController.cs
@@ -1,17 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Repositories;
+using InventoryManagementSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryManagementSystem.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductController(ProductRepository productRepository, IMapper mapper)
         {
-            return View();
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            var productModels = _mapper.Map<List<ProductModel>>(products);
+            return View(productModels);
         }
 
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            model.CategoryId = 1; // Default CategoryId for simplicity
+            var product = _mapper.Map<Product>(model);
+            await _productRepository.AddAsync(product);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Update(int id)
--- InventoryManagementSystem/ViewModels/ProductModel.cs
+++ InventoryManagementSystem/ViewModels/ProductModel.cs
@@ -1,15 +1,22 @@
 ﻿using InventoryManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace InventoryManagementSystem.ViewModels
 {
     public class ProductModel : BaseModel
     {
+        [Required(ErrorMessage="Name is required")]
         public string Name { get; set; }
-        public string Description { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Stock Quantity is required")]
         public int StockQuantity { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
 
     }
 }

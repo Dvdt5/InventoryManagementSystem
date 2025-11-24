632f47c90a26ddadec1471b7d047246044851659
dvdt5 <dagtekindavut07@gmail.com>
2025-11-24 14:12:32 +0300

    finished Inventory stock

--- InventoryManagementSystem/Controllers/InventoryStockController.cs
+++ InventoryManagementSystem/Controllers/InventoryStockController.cs
@@ -61,14 +61,76 @@ namespace InventoryManagementSystem.Controllers
             {
                 return View(model);
             }
+            var product = await _productRepository.GetByIdAsync(model.ProductId);
+            var warehouse = await _warehouseRepository.GetByIdAsync(model.WarehouseId);
+
             model.Created = DateTime.Now;
             model.Updated = DateTime.Now;
             model.LastUpdated = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            model.ProductName = product.Name;
+            model.WarehouseName = warehouse.Name;
+
             var inventoryStock = _mapper.Map<InventoryStock>(model);
             await _inventoryStockRepository.AddAsync(inventoryStock);
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> Update(int id)
+        {
+            var inventoryStock = await _inventoryStockRepository.GetByIdAsync(id);
+            var inventoryStockModel = _mapper.Map<InventoryStockModel>(inventoryStock);
 
+            var products = await _productRepository.GetAllAsync();
+            var productModels = _mapper.Map<List<ProductModel>>(products);
+
+            var warehouses = await _warehouseRepository.GetAllAsync();
+            var warehouseModels = _mapper.Map<List<WarehouseModel>>(warehouses);
+
+            var productSelectList = productModels.Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+
+            var warehouseSelectList = warehouseModels.Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+
+            ViewBag.Products = productSelectList;
+            ViewBag.Warehouses = warehouseSelectList;
+
+            return View(inventoryStockModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(InventoryStockModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            model.Updated = DateTime.Now;
+            model.LastUpdated = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var inventoryStock = _mapper.Map<InventoryStock>(model);
+            await _inventoryStockRepository.UpdateAsync(inventoryStock);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var inventoryStock = await _inventoryStockRepository.GetByIdAsync(id);
+            var inventoryStockModel = _mapper.Map<InventoryStockModel>(inventoryStock);
+            return View(inventoryStockModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(InventoryStockModel model)
+        {
+            var inventoryStock = _mapper.Map<InventoryStock>(model);
+            await _inventoryStockRepository.DeleteAsync(inventoryStock.Id);
+            return RedirectToAction("Index");
+        }
     }
 }
--- InventoryManagementSystem/ViewModels/InventoryStockModel.cs
+++ InventoryManagementSystem/ViewModels/InventoryStockModel.cs
@@ -5,7 +5,9 @@ namespace InventoryManagementSystem.ViewModels
     public class InventoryStockModel : BaseModel
     {
         public int ProductId { get; set; }
+        public string ProductName { get; set; }
         public int WarehouseId { get; set; }
+        public string WarehouseName { get; set; }
         public int QuantityOnHand { get; set; }
         public int QuantityReserved { get; set; }
         public string LastUpdated { get; set; }
